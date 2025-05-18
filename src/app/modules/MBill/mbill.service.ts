45fa21bcf9a8fc5ef8152aac2323d7df9df8c242
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2025-05-18 20:20:31 +0600

    #

--- src/app/modules/MBill/mbill.service.ts
+++ src/app/modules/MBill/mbill.service.ts
@@ -6,125 +6,26 @@ import { mBillSearchableFields } from './mbill.constant'
 import { MBill } from './mbill.model'
 
 const createMBillIntoDB = async () => {
-  const bill = {
-    _id: '6828c7ee6ada329f9f93a519',
-    monthName: 'February',
-  }
-  const user = {
-    _id: '682889585f4584f4517e690c',
-    userId: '6057',
-    mobile: '01622006057',
-    area: 'karatiya',
-  }
-
-  const data = {
-    bill: bill._id,
-    monthName: bill.monthName,
-    user: user?._id,
-    mobile: user?.mobile,
-    userId: user?.userId,
-    area: user?.area,
-  }
-
-  const result = await MBill.create(data)
-
-  return result
-
-  // const session = await startSession()
-
-  // try {
-  //   const result = await session.withTransaction(async () => {
-  //     const isUserExists = await User.findOne({
-  //       email: user?.email,
-  //     }).session(session)
-
-  //     if (!isUserExists) {
-  //       throw new AppError(
-  //         httpStatus.NOT_FOUND,
-  //         'User not found',
-  //         'User not found!',
-  //       )
-  //     }
-
-  //     // check meal exist
-  //     const isMealExist = await Meal.findById(id).session(session)
-  //     if (!isMealExist) {
-  //       throw new AppError(
-  //         httpStatus.NOT_FOUND,
-  //         'Meal not found',
-  //         'Meal not found!',
-  //       )
-  //     }
-
-  //     // balance check
-  //     if (isMealExist?.price < 1) {
-  //       throw new AppError(
-  //         httpStatus.BAD_REQUEST,
-  //         'Price should be grater then 0',
-  //         'Price should be grater then 0',
-  //       )
-  //     }
-
-  //     if (isMealExist?.stock < 1) {
-  //       throw new AppError(
-  //         httpStatus.BAD_REQUEST,
-  //         'Stock Out',
-  //         'No meal available',
-  //       )
-  //     }
-
-  //     // Decrease quantity
-
-  //     const mealQuantity = await Meal.findByIdAndUpdate(
-  //       isMealExist?._id,
-  //       {
-  //         stock: isMealExist?.stock - 1,
-  //       },
-  //       { session, new: true },
-  //     )
-
-  //     if (!mealQuantity) {
-  //       throw new AppError(
-  //         httpStatus.INTERNAL_SERVER_ERROR,
-  //         "Can't add order",
-  //         "Can't add order",
-  //       )
-  //     }
-
-  //     // Cut balance
-
-  //     // Find the last order to generate uId
-  //     const lastOrder = await Order.findOne()
-  //       .sort({ createdAt: -1 })
-  //       .select('uId')
-  //       .session(session)
-
-  //     const newUId = lastOrder ? Number(lastOrder.uId) + 1 : 1
-
-  //     // data
-  //     const data = {
-  //       user: isUserExists?._id,
-  //       name: isMealExist?.name,
-  //       description: isMealExist?.description,
-  //       price: isMealExist?.price,
-  //       type: isMealExist?.type,
-  //       uId: newUId,
-  //       userId: isUserExists?.userId,
-  //     }
-
-  //     const orderResult = await Order.create([data], { session })
-  //     return orderResult[0]
-  //   })
-  //   return result
-  // } catch (error: any) {
-  //   throw new AppError(
-  //     httpStatus.BAD_REQUEST,
-  //     error?.message || 'Order Failed',
-  //     error?.errorMessage || 'Order Failed',
-  //   )
-  // } finally {
-  //   session.endSession()
+  // const bill = {
+  //   _id: '6828c7ee6ada329f9f93a519',
+  //   monthName: 'February',
   // }
+  // const user = {
+  //   _id: '682889585f4584f4517e690c',
+  //   userId: '6057',
+  //   mobile: '01622006057',
+  //   area: 'karatiya',
+  // }
+  // const data = {
+  //   bill: bill._id,
+  //   monthName: bill.monthName,
+  //   user: user?._id,
+  //   mobile: user?.mobile,
+  //   userId: user?.userId,
+  //   area: user?.area,
+  // }
+  // const result = await MBill.create(data)
+  // return result
 }
 
 const getMBillFromDB = async (
